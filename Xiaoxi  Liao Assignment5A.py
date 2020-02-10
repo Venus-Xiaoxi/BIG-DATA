8d961e26a93b8c2a4c81a0b9fae9485e5237fc50
Venus-Xiaoxi <60410165+Venus-Xiaoxi@users.noreply.github.com>
2020-02-10 17:54:58 -0500

    Add files via upload

---
+++ Xiaoxi  Liao Assignment5A.py
@@ -0,0 +1,17 @@
+ # Xiaoxi Liao Assignment5A
+
+rate = float(input('input rate in decimal format:'))
+cashflow = input('Please enter a list of cashflow for years in sequences : ').split(sep=',')
+cashflows = list(map(int, cashflow))
+print(cashflows)
+
+NPV = []
+for x in cashflows:
+    for y in range(1, len(cashflows)+1):
+        npv = (x / ((1 + rate) ** y))
+        print((1 + rate) ** y)
+        print(npv)
+        NPV.append(npv)
+        break
+    print(NPV)
+    print(round(sum(NPV),2))
